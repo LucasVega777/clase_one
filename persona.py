0116099eb3163ef313f1ce92c6791cb6dd7f1594
Lucas Vega <lucasmvegap@gmail.com>
2019-04-24 18:09:04 -0400

    se anhadio una practica de programacion orientada a objetos

--- persona.py
+++ persona.py
@@ -9,16 +9,20 @@ class Persona:
         self.nombre = un_nombre
         self.edad = una_edad
         print("Hola soy", self.nombre, "y tengo ", self.edad, "anhos")
-    """def get_nombre_persona(self):
-        return self.nombre
-    def set_nombre(self):
-        self.nombre = un_nombre"""
+    def get_edad(self):
+        return self.edad
+    def cumple_anhos(self):
+        self.edad = self.edad + 1
+        return self.edad
+
 
 persona1 = Persona("Lucas", 20)
+print(persona1.cumple_anhos())
+persona1.get_edad()
 
 
+#modificar la clase de persona agregarle 
+#un atributo de edad y un metodo cumple_anhos y un metodo get_edad
+#inicializar un objeto de tipo persona 
+#y hacerle cumplir anhos
 
-
-
-
-def 
